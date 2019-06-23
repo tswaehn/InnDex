ca1b48c5ea584819f838c4cd8f0ccfead61c8cb9
tswaehn <sven.ginka@gmail.com>
2019-06-23 15:35:09 +0200

    ignore symlinks

--- InnDex.py
+++ InnDex.py
@@ -1,22 +1,26 @@
+import sys
+## check version
+if (sys.version_info < (3, 0)):
+    print("err: python 3.0 needed -- exit")
+    exit(1)
+    
 import createIndex
 import procDuplicates
 
-
-
-
 print("start app")
+
 
 dirName = '/home/tswaehn/Downloads'
 ##dirName= '/run/media/tswaehn/My Passport/data/backup_data/data';
 ##dirName= '/run/media/tswaehn/c587a5e7-5b5a-4c0c-857d-ba4cc1872b47/data/';
 
-##innDex= createIndex.new(dirName)
+innDex= createIndex.new(dirName)
 
 innDex= createIndex.load(dirName)
 
 print("index done")    
 
-procDuplicates.run( dirName,  innDex )
+#procDuplicates.run( dirName,  innDex )
 
     
 print("app end")
--- createIndex.py
+++ createIndex.py
@@ -1,16 +1,22 @@
+import sys
 import os
 import hashlib
 import indexReadWrite
+import time
+import glob
+
+# const
+__FNAME= "index.JSON"
 
 def __md5(fname):
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+        for chunk in iter(lambda: f.read(4096000000), b""):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
     
 
-def __createIndexOfDirectory(rootDir,  dirName):
+def __createIndexOfDirectory(rootDir,  dirName,  totalSize):
     # create a list of file and sub directories 
     # names in the given directory 
     listOfFile = os.listdir(dirName)
@@ -19,13 +25,18 @@ def __createIndexOfDirectory(rootDir,  dirName):
     for entry in listOfFile:
         # Create full path
         fullPath = os.path.join(dirName, entry)
+        # check for links
+        if os.path.islink(fullPath):
+            print("skipping symlink " + fullPath)
+            continue
         # If entry is a directory then get the list of files in this directory 
         if os.path.isdir(fullPath):
-            allFiles = allFiles + __createIndexOfDirectory(rootDir,  fullPath)
+            allFiles = allFiles + __createIndexOfDirectory(rootDir,  fullPath,  totalSize)
         else:
             # create tuple for list
             relpath=os.path.relpath(fullPath,  rootDir)
-            md5sum= __md5( fullPath )
+            #md5sum= __md5( fullPath )
+            md5sum= 0
             lastModTimeStamp= os.path.getmtime(fullPath)
             st = os.stat(fullPath)
             fsize= st.st_size
@@ -33,23 +44,58 @@ def __createIndexOfDirectory(rootDir,  dirName):
             # add to list
             allFiles.append( tuple )
             # debug
-            print(tuple)
-                
-    return allFiles
-    
-    
+            totalSize +=fsize
+            sys.stdout.write('.')
+            print("\rfound "+str(len(allFiles))+ " files",  end="\r")
+            ##print(tuple)
 
+    if (dirName == rootDir):
+        print("\r\n")
+    return allFiles
+
+   
+def __getIndexFileList( dirName ):    
+    fileList= list()
+    time.time()
+    return fileList
+
+
+def __getLatestIndexFile( dirName,  fname ):
+    fullpath=os.path.join(dirName, fname)
+    listOfFiles = glob.glob(fullpath + '.*')
+    x= 0
+    b = 0
+    for entry in listOfFiles:
+        print (entry)
+        if x==0:
+            x= entry
+            b= os.path.getmtime(entry)
+            continue
+        # lastModTimeStamp
+        a= os.path.getmtime(entry)
+        if (a > b):
+            b= a
+            x= entry
+    
+    fname= os.path.basename( x )
+    print("loading file")
+    print(fname)
+    return fname
+    
+    
 def new( dirName ):   
-    fname = 'index.JSON'
+    # get timestamp
+    ts= time.time()
+    timstampStr= str( int( ts ) )
+    
+    # create filename
+    fname = __FNAME + "." +timstampStr
     fullpath=os.path.join(dirName, fname)
     
-    
     # create index of files
-    allFiles= __createIndexOfDirectory( dirName,  dirName )
+    allFiles= __createIndexOfDirectory( dirName,  dirName,  0 )
     
     # prepare innDex object 
-    import time
-    ts = time.time()
     header={'fname':fname,  'type':'list',  'listlen': len( allFiles ),  'version':'0.1',  'timestamp':ts}
     innDex={'header':header,  'data':allFiles}
 
@@ -61,7 +107,7 @@ def new( dirName ):
     
 
 def load( dirName  ):    
-    fname = 'index.JSON'
+    fname= __getLatestIndexFile( dirName,  __FNAME )
     fullpath=os.path.join(dirName, fname)
     
     # read list from file
