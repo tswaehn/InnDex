cf7b88544840a076f7f2753dd786cfad725c9272
tswaehn <sven.ginka@gmail.com>
2019-06-24 18:00:27 +0200

    temp-backup every 60sec

--- InnDex.py
+++ InnDex.py
@@ -11,14 +11,17 @@ print("start app")
 
 
 dirName = '/home/tswaehn/Downloads'
+dirName= '/run/media/tswaehn/My Passport/data/backup_data/data';
+#dirName= '/run/media/tswaehn/c587a5e7-5b5a-4c0c-857d-ba4cc1872b47/data/';
+#dirName= '/home/tswaehn'
 
-#innDex= createIndex.new(dirName)
+innDex= createIndex.new(dirName)
 
 innDex= createIndex.load(dirName)
 
 print("index done")    
 
-procDuplicates.run( dirName,  innDex )
+#procDuplicates.run( dirName,  innDex )
 
     
 print("app end")
