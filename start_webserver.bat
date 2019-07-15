5ba82ab0628819fdd1bc451c79d37216e5b03187
Sven Ginka <sven.ginka@watttron.de>
2019-07-15 19:11:01 +0200

    add: minimal webserver

---
+++ start_webserver.bat
@@ -0,0 +1,3 @@
+
+cd webserver
+python run.py
---
+++ webserver/run.py
@@ -0,0 +1,55 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+from io import BytesIO
+import urllib, json
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_GET(self):
+        parsed_path = urllib.parse.urlparse(self.path)
+        message = '\n'.join([
+            'CLIENT VALUES:',
+            'client_address=%s (%s)' % (self.client_address,
+                                        self.address_string()),
+            'command=%s' % self.command,
+            'path=%s' % self.path,
+            'real path=%s' % parsed_path.path,
+            'query=%s' % parsed_path.query,
+            'request_version=%s' % self.request_version,
+            '',
+            'SERVER VALUES:',
+            'server_version=%s' % self.server_version,
+            'sys_version=%s' % self.sys_version,
+            'protocol_version=%s' % self.protocol_version,
+            '',
+        ])
+
+        print(message)
+
+        self._set_headers()
+        self.wfile.write(b'Hello, world!')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+
+        self._set_headers()
+
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
